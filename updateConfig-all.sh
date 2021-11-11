538529a1c952e598d3eddab3d9f36f5951525637
dev7ba <dev7ba@protonmail.com>
2021-11-11 12:27:45 +0100

    updateConfig-all now updates only services without redundancy

--- updateConfig-all.sh
+++ updateConfig-all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-curl -X POST http://gateway:8080/actuator/refresh & \
-curl -X POST http://bitcoindAdapter1:8080/actuator/refresh & \
-curl -X POST http://bitcoindAdapter2:8080/actuator/refresh & \
-curl -X POST http://txMemPool1:8080/actuator/refresh & \
-curl -X POST http://txMemPool2:8080/actuator/refresh & \
-curl -X POST http://txMemPool3:8080/actuator/refresh & \
-curl -X POST http://mempoolRecorder:8080/actuator/refresh
+curl -k -X POST https://gateway:8081/actuator/refresh & \
+curl -X POST http://bitcoindAdapter1:8081/actuator/refresh & \
+#curl -X POST http://bitcoindAdapter2:8081/actuator/refresh & \
+curl -X POST http://txMemPool1:8081/actuator/refresh & \
+#curl -X POST http://txMemPool2:8081/actuator/refresh & \
+#curl -X POST http://txMemPool3:8081/actuator/refresh & \
+#curl -X POST http://mempoolRecorder:8081/actuator/refresh
