589e92252afb8df9a733ce05774b8259876e5231
dev7ba <dev7ba@protonmail.com>
2022-01-19 11:05:55 +0100

    fixed: Added docker utilities to work with/without permissions

--- sudolessdocker.sh
+++
@@ -1,4 +0,0 @@
-#Allows the execution of docker without sudo.
-#As this has security implications use sudomoredocker.sh after this asap
-sudo adduser $USER docker
-sudo newgrp docker
--- sudomoredocker.sh
+++
@@ -1,2 +0,0 @@
-deluser $USER docker
-echo "Please, exit this terminal to reload group changes."
